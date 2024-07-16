b1602974de2e259e3d654d71930231c43aee34f8
giulio schintu <giuliom.schintu@gmail.com>
2024-07-16 21:50:30 +0200

    nothing of value

--- bitmap.c
+++ bitmap.c
@@ -14,7 +14,7 @@ void BitMap_init(BitMap* bit_map ,int num_bits, uint8_t* buffer) {
 void BitMap_setBit(BitMap*bit_map, int bit_num, int status){
     int byte_num = bit_num>>3;
     assert(byte_num<bit_map->buffer_size);
-    int bit_in_byte=byte_num&0x03;
+    int bit_in_byte=byte_num&0x07;
     if (status) {
     bit_map->buffer[byte_num] |= (1<<bit_in_byte);
   } else {
@@ -25,7 +25,7 @@ void BitMap_setBit(BitMap*bit_map, int bit_num, int status){
 int BitMap_bit(const BitMap* bit_map, int bit_num){
   int byte_num=bit_num>>3; 
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
+  int bit_in_byte=byte_num&0x07;
   return (bit_map->buffer[byte_num] & (1<<bit_in_byte))!=0;
 }
 
